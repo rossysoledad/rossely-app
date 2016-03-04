fe72faea01eb88af29c4c39d83d7256206988400
rosely <roselycallo@gmail.com>
2016-03-04 10:46:03 -0500

    creando conocimientos

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+
+	<div class="contenedor">
+
+	<nav>
+		<ul class="menu-lista">
+		<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+            <li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+            <li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+            <li class="menu-item"><a href="" class="menu-link">ANUNCIO</a></li>
+            <li class="menu-item"><a href="" class="menu-link">CONTACTENOS</a></li>
+            </ul>
+	</nav>
+
+
+	<h1 class="morado">EMPRESA COCA COLA</h1>
+
+	<p class="azul">Una de las compañías más reconocidas a nivel mundial y que ha sabido mantener y revitalizar su marca. Su anuncio en el Super Bowl, en el que reflejaba a un Estados Unidos multicultural, fue criticado por los conservadores pero admirado por aquellos que creen que la compañía de Atlanta es un bastión de la igualdad… ¡Nada mal para un fabricante de refrescos! (puntos extra a su equipo de marketing). Además, ha sabido expander su línea de productos.
+	</p>
+		
+		<img src="6.jpg" height="400" width="900" >
+	    </div class="slider">
+	    <img clas="rosely" height="180" ="" width="180" >
+
+ 	    <img class="rosely" src="1.jpg" height="180" width="180">
+ 	    <img class="rosely" src="2.jpg" height="180" width="180">
+ 	    <img class="rosely" src="4.jpg" height="180" width="180">
+ 	    <img class="rosely" src="5.jpg" height="180" width="180">
+	    <div>
+		
+	</div>
+	
+    </body>
+    </html>
